e24702e6c653fbe2f213fb83d5f1f42f459faa0d
David <G00422935@atu.ie>
2023-10-24 09:16:25 +0100

    displaying student info

--- src/main/java/ie/atu/StudentDatabase.java
+++ src/main/java/ie/atu/StudentDatabase.java
@@ -5,4 +5,8 @@ public class StudentDatabase {
     String StudentEmail;
     String CourseName;
 
+    public void displayInfo()
+    {
+        System.out.println("Enter you name\n" + StudentName + "Enter your student email\n" + StudentEmail + "Enter your Course Name\n" + CourseName);
+    }
 }
