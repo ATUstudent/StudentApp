4f627e39933e6055bb2793e325e7633291ad0cc4
David <G00422935@atu.ie>
2023-10-24 09:34:03 +0100

    added constructors

--- src/main/java/ie/atu/StudentDatabase.java
+++ src/main/java/ie/atu/StudentDatabase.java
@@ -5,6 +5,12 @@ public class StudentDatabase {
     String StudentEmail;
     String CourseName;
 
+    public StudentDatabase(String studentName, String studentEmail, String courseName) {
+        StudentName  =  studentName;
+        StudentEmail = studentEmail;
+        CourseName   =   courseName;
+    }
+
     public void displayInfo()
     {
         System.out.println("Enter you name\n" + StudentName + "Enter your student email\n" + StudentEmail + "Enter your Course Name\n" + CourseName);
